a6c6728cf5e57b342716120a90f6ad3f23a733e6
zktosu <zktosu@gmail.com>
2023-09-02 22:56:38 +0300

    initial attempt to create simple manu

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-# imagine a terminal based local library app
-# create terminal ui -- explore tools used in python
-# option 1 simple-term-menu
-# -- have fun with git 
+from simple_term_menu import TerminalMenu
+
+def main():
+	options = ["Add Book", "Delete Book", "Issue Book"]
+	terminal_menu = TerminalMenu(options)
+	selected_index = terminal_menu.show()
+	# selected menu is index number,
+	# if esc pressed then value is None
+	print(f"seçilen menu index i {selected_index}")
+
+if __name__ == "__main__":
+	main()
