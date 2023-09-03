58cab5aa12ca740c645c6ff465d09e7bf620dbf7
zktosu <zktosu@gmail.com>
2023-09-03 13:51:33 +0300

    added database insertion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from simple_term_menu import TerminalMenu
-
+import sqlite3
 def main():
 	options = ["Add Book", "Delete Book", "Issue Book"]
 	terminal_menu = TerminalMenu(options)
@@ -7,8 +7,15 @@ def main():
 	# selected menu is index number,
 	# if esc pressed then value is None
 	# print(f"seçilen menu index i {selected_index}")
+	con = sqlite3.connect("database.db")	
+	cur = con.cursor()
 	if selected_index == 0:
-		print("added book")
+		# insert item into database.
+		title= input("title ")
+		author = input("author ")
+		stmt = ''' INSERT INTO books(title,author) VALUES(?,?);'''
+		cur.execute(stmt,(title, author))
+		con.commit()
 	if selected_index == 1:
 		print("deleted the book")
 	if selected_index == 2:
